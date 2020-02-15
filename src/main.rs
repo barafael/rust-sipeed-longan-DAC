e1f248835e6030d5ae2d9cb3d3f45fcdee1fdc7d
Rafael Bachmann <mediumendian@gmail.com>
2020-02-15 17:12:34 +0100

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,48 @@
-#![feature(global_asm)]
-#![no_main]
 #![no_std]
+#![no_main]
 
-use panic_abort;
+use panic_halt as _;
 
-mod rcu;
-mod gpio;
-mod dac;
-mod register_helpers;
+use riscv_rt::entry;
+use gd32vf103xx_hal as hal;
+use hal::pac as pac;
+use gd32vf103xx_hal::gpio::GpioExt;
+use longan_nano::led::{Led, rgb};
+use gd32vf103xx_hal::prelude::*;
+use longan_nano::sprintln;
+use gd32vf103xx_hal::rcu::RcuExt;
+use gd32vf103xx_hal::delay::McycleDelay;
+use embedded_hal::blocking::delay::DelayMs;
 
-use rcu::*;
-use gpio::*;
-use dac::*;
-pub use register_helpers::*;
-
-fn rcu_config() {
-    rcu_periph_clock_enable(RCU_GPIOA);
-    rcu_periph_clock_enable(RCU_GPIOC);
-    rcu_periph_clock_enable(RCU_DAC);
-}
-
-fn gpio_config() {
-    const GPIO_PIN_4: u32 = bit(4);
-    gpio_init(GPIOA, GPIO_MODE_AIN, GPIO_OSPEED_50MHZ, GPIO_PIN_4);
-}
-
-fn dac_config() {
-    dac_deinit();
-    dac_trigger_source_config(DAC0, DAC_TRIGGER_SOFTWARE);
-    dac_trigger_enable(DAC0);
-    dac_wave_mode_config(DAC0, DAC_WAVE_DISABLE);
-    dac_output_buffer_enable(DAC0);
-    dac_enable(DAC0);
-}
-
-// The reset handler
-#[no_mangle]
-pub unsafe extern "C" fn Reset() -> ! {
-    r0::zero_bss(&mut _sbss, &mut _ebss);
-    r0::init_data(&mut _sdata, &mut _edata, &_sidata);
-    main()
-}
-
-// don't compile with optimization enabled!
-fn delay(mut n: u32) {
-    while n != 0 {
-        n -= 1;
-    }
-}
-
+#[entry]
 fn main() -> ! {
-    rcu_config();
-    gpio_config();
-    dac_config();
+    let dp = pac::Peripherals::take().unwrap();
+    let mut rcu = dp.RCU.configure()
+        .ext_hf_clock(8.mhz())
+        .sysclk(108.mhz())
+        .freeze();
 
-    let mut dac_output: u16 = 0;
+    let gpioa = dp.GPIOA.split(&mut rcu);
+    longan_nano::stdout::configure(dp.USART0, gpioa.pa9, gpioa.pa10, 115_200.bps(), &mut rcu);
+
+    sprintln!("Hello Longan");
+
+    let gpioc = dp.GPIOC.split(&mut rcu);
+
+    let (mut red, mut green, mut blue) = rgb(gpioc.pc13, gpioa.pa1, gpioa.pa2);
+    let leds: [&mut dyn Led; 3] = [&mut red, &mut green, &mut blue];
+
+    let mut delay = McycleDelay::new(&rcu.clocks);
+
+    let dac = dp.DAC;
+    let mut i = 0;
     loop {
-        if dac_output >= 4096 {
-            dac_output = 0;
-        } else {
-            dac_output += 25;
-        }
-        dac_data_set(DAC0, DAC_ALIGN_12B_R, dac_output);
-        dac_software_trigger_enable(DAC0);
-        //delay(0xf);
+        let inext = (i + 1) % leds.len();
+        sprintln!("Colors! {}", i);
+        leds[i].off();
+        leds[inext].on();
+        delay.delay_ms(500);
+
+        i = inext;
     }
 }
-
-extern "C" {
-    // Boundaries of the .bss section
-    static mut _ebss: u32;
-    static mut _sbss: u32;
-
-    // Boundaries of the .data section
-    static mut _edata: u32;
-    static mut _sdata: u32;
-
-    // Initial values of the .data section (stored in Flash)
-    static _sidata: u32;
-}
-
-// Make sure there is an abort when linking
-#[cfg(target_arch = "riscv32")]
-global_asm!(
-    r#"
-lui sp, %hi(__stacktop)
-call Reset
-.globl abort
-abort:
-  jal zero, abort
-"#
-);
